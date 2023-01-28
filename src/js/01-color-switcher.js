97bd46ad0d2a02e3526a2f2bd5faba0d3ba48081
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-28 22:20:26 +0200

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onSwitcherStart);
+refs.stopBtn.addEventListener('click', onSwitcherStop);
+
+let setId = null;
+
+function onSwitcherStart() {
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled', true);
+    setId = setInterval(() => { document.body.style.backgroundColor = `${getRandomHexColor()}` }, 1000);
+}
+
+function onSwitcherStop() {
+    refs.startBtn.removeAttribute('disabled', true);
+    refs.stopBtn.setAttribute('disabled', true);
+    clearInterval(setId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

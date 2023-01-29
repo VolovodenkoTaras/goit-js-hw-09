acc94effcfad0d7069af51557dd1beea97d4469f
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-29 21:51:36 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,7 +5,7 @@ import Notiflix from 'notiflix';
 require('flatpickr/dist/themes/material_orange.css');
 
 let setTimerId = null;
-let dateDiff = null;
+let timeToDate = null;
 
 const refs = {
     inputCalendar: document.querySelector('input[type="text"]'),
@@ -24,26 +24,18 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        clearInterval(setTimerId);
-        const setId = setInterval(() => {
-            dateDiff = selectedDates[0].getTime() - new Date().getTime();
-            if (dateDiff <= 0) {
-                clearInterval(setId);
-                refs.startBtn.disabled = true;
-                Notiflix.Notify.failure('Please choose a date in the future', {
-                    opacity: 1,
-                    position: 'center-top',
-                    timeout: 250,
-                    backOverlay: true,
-                    cssAnimationDuration: 3500,
-                    backOverlayColor: 'rgba(255,85,73,0.2)',
-                    cssAnimationStyle: 'from-top',
-                });
-                return;
-            }
-        }, 1000);
-
-        refs.startBtn.disabled = false;
+        timeToDate = selectedDates[0].getTime()
+        if (timeToDate > Date.now()) {
+            refs.startBtn.removeAttribute('disabled');
+        } else {
+            Notiflix.Notify.failure('Please choose a date in the future', {
+                opacity: 1,
+                position: 'center-top',
+                timeout: 100,
+                cssAnimationDuration: 2000,
+                cssAnimationStyle: 'from-top',
+            });
+        }
     },
 };
 
@@ -56,23 +48,18 @@ function onClickTimer() {
         opacity: 1,
         position: 'center-top',
         timeout: 250,
-        cssAnimationDuration: 1500,
-        backOverlay: true,
-        backOverlayColor: 'rgba(50,198,130,0.2)',
+        cssAnimationDuration: 2000,
         cssAnimationStyle: 'from-top',
     });
-    const setTimerId = setInterval(() => {
-        if (dateDiff > 0) {
-            const { days, hours, minutes, seconds } = convertMs(dateDiff);
-
+    setTimerId = setInterval(() => {
+        const deltaTime = timeToDate - Date.now();
+        if (deltaTime > 0) {
+            const { days, hours, minutes, seconds } = convertMs(deltaTime);
             refs.timerDays.textContent = `${days}`;
             refs.timerHours.textContent = `${hours}`;
             refs.timerMinutes.textContent = `${minutes}`;
             refs.timerSeconds.textContent = `${seconds}`;
-        }
-        if (dateDiff <= 0) {
-            clearInterval(setTimerId);
-        }
+        } else { clearInterval(setTimerId) }
     }, 1000);
     refs.startBtn.disabled = true;
     refs.inputCalendar.disabled = true;
